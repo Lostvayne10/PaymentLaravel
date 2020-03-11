7368ba45b3175735428db8578bb2805d2d2dbe54
Alejandro infante <antonio.infante@alumnos.udg.mx>
2020-03-11 13:55:46 -0600

    batallando con el error del ssl 56

---
+++ resources/views/components/stripe-collapse.blade.php
@@ -0,0 +1,3 @@
+<small>You will be redirected to the stripe checkoutpage</small>
+<input type="text" name="test">
+<input type="text" name="tes2"> 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,13 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Make a payment</div>
-                    <form action="" method="POST" id="paymentform">
+                  
+                <div class="card-body">
+                <form action="{{ route('pay')}}" method="POST" id="paymentform">
                         @csrf 
 
                         <div class="row">
-                            <div class="col- offset-md-1">
+                            <div class="col-auto ">
                                 <label >How mucho you want to pay?</label>
                                 <input type="number" 
                                 name="value" 
@@ -34,6 +36,30 @@
                                         </option>
                                     @endforeach
                                 </Select>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col">
+                                <label >Select the desired payment platform:</label>
+                                <div class="form-group" id="toggler">
+                                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                                        @foreach ($paymentPlatforms as $paymentplatform)
+                                            <label class="btn btn-outline-secondary rounded m-2 p-1"
+                                                    data-target="#{{ $paymentplatform->name }}Collapse"
+                                                    data-toggle="collapse">
+                                                <input type="radio" name="payment_platform" value="{{ $paymentplatform->id }}" required>
+                                                <img class="img-thumbnail" src="{{ $paymentplatform->image }}" alt="">
+                                            </label>
+                                        @endforeach
+                                    </div>
+                                    @foreach ($paymentPlatforms as $paymentplatform)
+                                        <div id="{{ $paymentplatform->name }}Collapse"
+                                        class="collapse"
+                                        data-parent="#toggler">
+                                            @include('components.' . strtolower($paymentplatform->name) . '-collapse')
+                                        </div>
+                                    @endforeach
+                                </div>
                             </div>
                         </div>
                         <div class="text-center mt-3">
